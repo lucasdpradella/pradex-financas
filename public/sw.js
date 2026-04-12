848233e777309ef6d0aab1693390762faac30806
lucasdpradella <lucasdpradella@gmail.com>
2026-04-11 22:06:35 -0300

    Update sw.js

--- public/sw.js
+++ public/sw.js
@@ -1,19 +1,8 @@
-const CACHE_NAME = 'pradex-v2';
-
-self.addEventListener('install', event => {
-  self.skipWaiting();
-});
-
+self.addEventListener('install', () => self.skipWaiting());
 self.addEventListener('activate', event => {
-  event.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(keys.map(key => caches.delete(key)))
-    )
-  );
+  event.waitUntil(caches.keys().then(keys => Promise.all(keys.map(k => caches.delete(k)))));
   self.clients.claim();
 });
-
 self.addEventListener('fetch', event => {
   event.respondWith(fetch(event.request));
-});
 });
